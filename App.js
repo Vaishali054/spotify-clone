08c0017960f5f05ac8adea4dc107ce651194bfd8
Vaishali054 <100770875+Vaishali054@users.noreply.github.com>
2023-07-04 12:03:52 +0530

    first commit

--- App.js
+++ App.js
@@ -1,10 +1,10 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-
+import { NavigationContainer } from '@react-navigation/native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text className='text-red-500'>hey there!</Text>
       <StatusBar style="auto" />
     </View>
   );
